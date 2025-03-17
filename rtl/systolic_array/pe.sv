28860e9f942a5764bf26a7703e76e62fe59faf87
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2025-03-17 22:49:05 +0800

    add valid bits to systolic array

--- rtl/systolic_array/pe.sv
+++ rtl/systolic_array/pe.sv
@@ -6,25 +6,30 @@ module pe #(
 
     // Data Inputs 
     input logic [DATA_WIDTH-1:0] i_ifmap, i_weight,
+    input logic i_ifmap_valid, i_weight_valid,
     input logic [DATA_WIDTH*2-1:0] i_psum,
+    input logic i_psum_valid,
 
     // Control Inputs
-
     input logic i_reg_clear, // Clear register
     input logic i_pe_en,  // Enable PE to perform multiply-and-accumulate
 
     // Enable one cycle after last computation to 
     // output partial sum to the next PE
     // Performs a shift operation to the left
-    input logic i_psum_out_en,
+    input logic i_psum_out_en, i_scan_en,
 
     // Data Outputs
     output logic [DATA_WIDTH-1:0] o_ifmap, o_weight,
-    output logic [DATA_WIDTH*2-1:0] o_ofmap
+    output logic o_ifmap_valid, o_weight_valid,
+    output logic [DATA_WIDTH*2-1:0] o_ofmap,
+    output logic o_ofmap_valid
 );
 
     logic [DATA_WIDTH-1:0] reg_ifmap, reg_weight;
+    logic reg_ifmap_valid, reg_weight_valid;
     logic [DATA_WIDTH*2-1:0] reg_psum, o_multiplier, reg_psum_out;
+    logic reg_psum_valid;
 
     always_ff @(posedge i_clk or negedge i_nrst) begin
         if (~i_nrst) begin
@@ -37,6 +42,8 @@ module pe #(
             end else if (i_pe_en) begin
                 reg_ifmap <= i_ifmap;
                 reg_weight <= i_weight;
+                reg_ifmap_valid <= i_ifmap_valid;
+                reg_weight_valid <= i_weight_valid;
             end
         end
     end
@@ -50,26 +57,41 @@ module pe #(
         .p(o_multiplier)
     );
 
-    // always_comb begin
-    //     o_multiplier = i_ifmap * i_weight;
-    // end
+    logic mac_en;
+    assign mac_en = i_pe_en & (reg_ifmap_valid & reg_weight_valid);
 
-    logic write_en;
-    assign write_en = i_pe_en & (reg_ifmap != 0 & reg_weight != 0);
-
+    // Multiplier and Accumulator
     always_ff @(posedge i_clk or negedge i_nrst) begin
         if(~i_nrst) begin
             reg_psum <= 0;
-            reg_psum_out <= 0;
         end else begin
-            if(i_psum_out_en) begin
-                reg_psum_out <= reg_psum;
-            end else if (i_reg_clear) begin
+            if (i_reg_clear) begin
                 reg_psum <= 0;
-                reg_psum_out <= 0;
-            end else if(i_pe_en) begin
+            end else if(mac_en) begin
                 reg_psum <= reg_psum + o_multiplier;
             end 
+        end
+    end
+
+    // Output partial sum to the next PE
+    always_ff @(posedge i_clk or negedge i_nrst) begin
+        if(~i_nrst) begin
+            reg_psum_out <= 0;
+            reg_psum_valid <= 0;
+        end else begin
+            if (i_reg_clear) begin
+                reg_psum_out <= 0;
+                reg_psum_valid <= 0;
+            end else if (i_scan_en) begin
+                reg_psum_out <= i_psum;
+                reg_psum_valid <= i_psum_valid;
+            end else if (i_psum_out_en) begin
+                reg_psum_out <= reg_psum;
+                reg_psum_valid <= reg_weight_valid & reg_ifmap_valid;
+            end else begin
+                reg_psum_out <= 0;
+                reg_psum_valid <= 0;
+            end
         end
     end
 
@@ -77,6 +99,9 @@ module pe #(
         o_ifmap = reg_ifmap;
         o_weight = reg_weight;
         o_ofmap = reg_psum_out;
+        o_ifmap_valid = reg_ifmap_valid;
+        o_weight_valid = reg_weight_valid;
+        o_ofmap_valid = reg_psum_valid;
     end
 
 endmodule
--- rtl/systolic_array/systolic_array.sv
+++ rtl/systolic_array/systolic_array.sv
@@ -3,22 +3,31 @@ module systolic_array #(
     parameter WIDTH = 2,
     parameter HEIGHT = 2
 ) (
-    input logic i_clk, i_nrst, i_reg_clear, i_pe_en, i_psum_out_en,
+    input logic i_clk, i_nrst, i_reg_clear, i_pe_en, i_psum_out_en, i_scan_en,
     input logic [1:0] i_mode,
-    input logic [0:HEIGHT-1][DATA_WIDTH-1:0] i_ifmap, 
+    input logic [0:HEIGHT-1][DATA_WIDTH-1:0] i_ifmap,
+    input logic [0:HEIGHT-1] i_ifmap_valid,
     input logic [0:WIDTH-1][DATA_WIDTH-1:0] i_weight,
-    output logic [0:HEIGHT-1][DATA_WIDTH*2-1:0] o_ofmap
+    input logic [0:WIDTH-1] i_weight_valid,
+    output logic [0:HEIGHT-1][DATA_WIDTH*2-1:0] o_ofmap,
+    output logic [0:HEIGHT-1] o_ofmap_valid
 );
     logic [0:HEIGHT-1][0:WIDTH][DATA_WIDTH-1:0] mat_A;
     logic [0:HEIGHT][0:WIDTH-1][DATA_WIDTH-1:0] mat_B;
     logic [0:HEIGHT-1][0:WIDTH][DATA_WIDTH*2-1:0] mat_C;
 
+    logic [0:HEIGHT-1][0:WIDTH] mat_A_valid;
+    logic [0:HEIGHT][0:WIDTH-1] mat_B_valid;
+    logic [0:HEIGHT-1][0:WIDTH] mat_C_valid;
+
     // Mapping of ifmap
     genvar jj;
     generate
         for (jj=0; jj < HEIGHT; jj++) begin : y_ios
-            assign mat_A[jj][0] = i_ifmap[jj]; 
+            assign mat_A[jj][0] = i_ifmap[jj];
+            assign mat_A_valid[jj][0] = i_ifmap_valid[jj];
             assign o_ofmap[jj] = mat_C[jj][0];
+            assign o_ofmap_valid[jj] = mat_C_valid[jj][0];
         end
     endgenerate
 
@@ -27,6 +36,14 @@ module systolic_array #(
     generate
         for (ii=0; ii < WIDTH; ii++) begin : x_ios
             assign mat_B[0][ii] = i_weight[ii];
+            assign mat_B_valid[0][ii] = i_weight_valid[ii];
+        end
+    endgenerate
+
+    generate
+        for (jj = 0; jj < HEIGHT; jj++) begin
+            assign mat_C[jj][WIDTH] = '0;
+            assign mat_C_valid[jj][WIDTH] = 0;
         end
     endgenerate
 
@@ -43,13 +60,20 @@ module systolic_array #(
                     .i_mode(i_mode),
                     .i_ifmap(mat_A[j][i]),
                     .i_weight(mat_B[j][i]),
-                    // .i_psum(mat_C[j][i+1]),
+                    .i_ifmap_valid(mat_A_valid[j][i]),
+                    .i_weight_valid(mat_B_valid[j][i]),
+                    .i_psum_valid(mat_C_valid[j][i+1]),
+                    .i_psum(mat_C[j][i+1]),
                     .i_reg_clear(i_reg_clear),
                     .i_pe_en(i_pe_en),
                     .i_psum_out_en(i_psum_out_en),
+                    .i_scan_en(i_scan_en),
                     .o_ifmap(mat_A[j][i+1]),
                     .o_weight(mat_B[j+1][i]),
-                    .o_ofmap(mat_C[j][i])
+                    .o_ifmap_valid(mat_A_valid[j][i+1]),
+                    .o_weight_valid(mat_B_valid[j+1][i]),
+                    .o_ofmap(mat_C[j][i]),
+                    .o_ofmap_valid(mat_C_valid[j][i])
                 );
             end
         end
