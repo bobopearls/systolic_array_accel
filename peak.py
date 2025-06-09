808356443cc1c4ec2749f9581131ae92904b1770
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2025-06-09 13:47:53 +0800

    update peak sim

--- peak.py
+++ peak.py
@@ -89,10 +89,11 @@ def generate_simv_command(
 
 def main():
     csv_path = "vww/metadata.csv"
-    spad_sizing = [(32,13), ]
-    dimensions = [8]
-    columns = [8]
-    fifo_depth = [16]
+
+    spad_sizing = [(128, 11)]
+    dimensions = [32]
+    columns = [32]
+    fifo_depth = [64]
     mpp_depth = 9
 
     for spad_data_width, addr_width in spad_sizing:
@@ -101,45 +102,52 @@ def main():
                     for depth in fifo_depth:
                         write_system_parameters(spad_data_width, addr_width, rows, rows, depth, mpp_depth)
                         # Synthesize design
-                        sim_command = "vcs tb_top.sv ../mapped/8_16_32.v /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/verilog/GF22FDX_SC7P5T_116CPP_BASE_CSC20L.v /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/verilog/prim.v -sverilog -full64 -debug_pp +neg_tchk -R -l vcs.log"
+                        sim_command = "vcs tb_top.sv ../mapped/8_16_32.v /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/verilog/GF22FDX_SC7P5T_116CPP_BASE_CSC20L.v /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/verilog/prim.v -sverilog -full64 -debug_pp +neg_tchk -l vcs.log"
                         subprocess.run(sim_command, shell=True)
                         print(f"Compilation completed for {rows}x{rows}x{depth} with SPAD bus width {spad_data_width}\n")
                         
-                        identifier = 1
-                        h = 8
-                        w = 8
-                        c_i = 16
-                        c_o = 8
-                        stride = 1
-                        type = "P"
-                        
-                        # 0 for Pointwise and 1 for Depthwise
-                        conv_mode = 0 if type == "P" else 1
-                        
-                        out_size = h if type == "P" else ((h-3) // stride) + 1
-                        i_filename = f"base_inputs.txt"
-                        w_filename = f"base_weights.txt"
-                        o_filename = f"data/out/{rows}_{rows}_{depth}_{spad_data_width}_output.txt"
+                        with open(csv_path, mode='r') as file:
+                            reader = csv.DictReader(file)
+                            for row in reader:
+                                identifier = row['Identifier']
+                                h = int(row['H/W'])
+                                w = int(row['H/W'])
+                                c_i = int(row['C'])
+                                c_o = int(row['Oc'])
+                                stride = int(row['Stride'])
+                                type = row['Type']
+                                
+                                # 0 for Pointwise and 1 for Depthwise
+                                conv_mode = 0 if type == "P" else 1
 
-                        for precision in [8]:
-                            cycle_file = f"cycles/peak_cycle.txt"
-                            tb_cmd = generate_simv_command(
-                                conv_mode,
-                                h,
-                                c_i,
-                                c_o,
-                                out_size,
-                                stride,
-                                precision,
-                                identifier,
-                                i_filename,
-                                w_filename,
-                                cycle_file,
-                                o_filename
-                            )
-                            print(tb_cmd)
-                            print(f"Processing {identifier} with {precision}-bit precision and dimensions {rows}x{cols}x{depth} and SPAD bus width {spad_data_width}\n")
-                            subprocess.run(tb_cmd, shell=True)
+                                if identifier != "10":
+                                    continue
+                                
+                                out_size = h if type == "P" else ((h-3) // stride) + 1
+                                i_filename = f"vww/{spad_data_width}_bits/inputs/{identifier}.txt"
+                                w_filename = f"vww/{spad_data_width}_bits/weights/{identifier}.txt"
+                                o_filename = f"data/out/{rows}_{rows}_{depth}_{spad_data_width}_output.txt"
+
+                                for precision in [2]:
+                                    cycle_file = f"cycles/{rows}_{rows}_{depth}_{spad_data_width}_cycle.txt"
+                                    tb_cmd = generate_simv_command(
+                                        conv_mode,
+                                        h,
+                                        c_i,
+                                        c_o,
+                                        out_size,
+                                        stride,
+                                        precision,
+                                        identifier,
+                                        i_filename,
+                                        w_filename,
+                                        cycle_file,
+                                        o_filename
+                                    )
+                                    print(tb_cmd)
+                                    print(f"Processing {identifier} with {precision}-bit precision and dimensions {rows}x{cols}x{depth} and SPAD bus width {spad_data_width}\n")
+                                    subprocess.run(tb_cmd, shell=True)
+
 
 
 if __name__ == "__main__":
--- power.tcl
+++ power.tcl
@@ -1,17 +1,17 @@
 set power_enable_analysis true
-set power_analysis_mode time_based
+set power_analysis_mode averaged
 set search_path "$search_path mapped lib cons rtl sim"
 set target_library /cad/tools/libraries/dwc_logic_in_gf22fdx_sc7p5t_116cpp_base_csc20l/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_FDK_RELV02R80/model/timing/db/GF22FDX_SC7P5T_116CPP_BASE_CSC20L_TT_0P80V_0P00V_0P00V_0P00V_25C.db
 set link_library "* $target_library"
 # Read the synthesized netlist
-read_verilog top_mapped.v
+read_verilog 32_64_128_mapped.v
 current_design top
 link_design
 # Define simulation environment
 set_units -time ps -resistance kOhm -capacitance fF -voltage V -current mA
 create_clock -period 10000 -name CLK [get_ports i_clk]
-read_vcd "top.dump" -strip_path "tb_top/dut"
+update_timing
+read_saif -strip_path tb_top/dut toggle_report.saif
 check_power
-set_power_analysis_options -waveform_format fsdb -waveform_output vcd
 update_power
-report_power -hierarchy > top_pt_power.rpt
+report_power -hierarchy > peak_pwise.rpt
