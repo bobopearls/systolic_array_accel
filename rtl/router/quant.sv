c647dce3bd7f735b299266f0be636857120d5978
aaronprincipe <johnaaronprincipe@gmail.com>
2026-05-09 22:52:18 +0800

    added bias

--- rtl/router/quant.sv
+++ rtl/router/quant.sv
@@ -3,27 +3,32 @@ module quant #(
 ) (
     input  logic i_clk, i_nrst, i_en, i_store_reg,
     input  logic [  DATA_WIDTH-1:0] i_sh,
-    input  logic [2*DATA_WIDTH-1:0] i_m0,
+    input  logic        [2*DATA_WIDTH-1:0] i_m0,
     input  logic signed [4*DATA_WIDTH-1:0] i_act,
-    input  logic signed [  DATA_WIDTH-1:0] i_zero_point,
+    input  logic signed [4*DATA_WIDTH-1:0] i_bias,
+    input  logic signed [  DATA_WIDTH-1:0] i_zero_point, // really output zero point, just named i_zero_point because it's an input to this module
     output logic signed [  DATA_WIDTH-1:0] o_act,
     output logic o_valid
 );
-    logic [  DATA_WIDTH-1:0] sh;
-    logic [2*DATA_WIDTH-1:0] m0;
+    logic        [  DATA_WIDTH-1:0] sh;
+    logic        [2*DATA_WIDTH-1:0] m0;
     logic signed [4*DATA_WIDTH-1:0] act;
-    logic signed [4*DATA_WIDTH-1:0] p, q;
+    logic signed [4*DATA_WIDTH-1:0] bias;
+    logic signed [4*DATA_WIDTH-1:0] q;
 
+    parameter int fixedPointBits = 2*DATA_WIDTH;
     always_ff @(posedge i_clk) begin
         if (!i_nrst) begin
             sh  <= 0;
             m0  <= 0;
+            bias <= 0;
             act <= 0;
             o_valid <= 0;
         end else begin
             if (i_store_reg) begin
                 sh <= i_sh;
                 m0 <= i_m0;
+                bias <= i_bias;
             end
             if (i_en) begin
                 act <= i_act;
@@ -35,8 +40,7 @@ module quant #(
         end
     end
 
-    assign p = m0 * act;
-    assign q = (i_zero_point) + (p >>> (2*DATA_WIDTH + sh));
+    assign q = i_zero_point + (($signed({1'b0, m0}) * (act + bias)) >>> (fixedPointBits + sh));
 
     always_comb begin
         if (q < -128) begin
